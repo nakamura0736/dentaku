3331e160d9c2391e1e9c93eb14d201110f69cd1f
nakamura0736 <53926917+nakamura0736@users.noreply.github.com>
2019-12-22 16:26:26 +0900

    Create dentaku.js

---
+++ dentaku.js
@@ -0,0 +1,71 @@
+(function(){
+	'user strict';//厳密なエラーチェック
+
+	var price = document.getElementById('price');
+	var num = document.getElementById('num');
+	var unit = document.getElementById('unit');
+	var btn = document.getElementById('btn');
+	var result = document.getElementById('result');
+	var reset = document.getElementById('reset');
+	var str;
+
+	function checkInput(){
+		// /^[1-9][0-9]*$/
+		if(
+			price.value.match(/^[1-9][0-9]*$/) !== null &&
+			num.value.match(/^[1-9][0-9]*$/) !== null
+		){
+			btn.classList.remove('disabled');
+		} else {
+			btn.classList.add('disabled');
+		}
+	}
+
+	// btn.addEventListener('click', function(){
+	// 	console.log('test,clicked');
+	// },false);
+
+	btn.addEventListener('click', function(){
+		var payLess;
+		var short;
+		var payMore;
+		var over;
+		if (this.classList.contains('disabled') === true) {
+			return;
+		}
+		// a. 300(payLess) .. 100(short)不足
+		// b. 400(payMore) .. 200(over) あまり
+		//payLess = 1000 / 3; // 333.333...
+		//payLess = 1000 / 3 / 100; //3.333
+		payLess = Math.floor(price.value / num.value / unit.value) * unit.value ; //300
+		short = price.value - (payLess * num.value); //100
+		payMore = Math.ceil(price.value / num.value / unit.value) * unit.value ; //400
+		over = Math.abs(price.value - (payMore * num.value)); // -200
+		if(over === 0 && short === 0){
+			str = '一人 ' + (price.value / num.value) + ' 円ちょうどです！(^^)b';
+			console.log('just');
+		}else {
+			str = 
+				'一人 ' + payLess + '円だと' + short + ' 円足りません。'  +   
+				'一人 ' + payMore + '円だと' + over + ' 円余ります。';
+
+			// console.log('clicked!!!!!');
+			// alert('clicked!!');
+		}
+		result.textContent = str;
+		reset.classList.remove('hidden');
+	});
+
+	price.addEventListener('keyup', checkInput);
+	num.addEventListener('keyup', checkInput);
+	reset.addEventListener('click', function(){
+		result.textContent = 'ここに結果を表示します(; ・ ω ・ )ﾄﾞｷﾄﾞｷ...'
+		price.value = '';
+		num.value = '';
+		unit.value = '100';
+		btn.classList.add('disabled');
+		this.classList.add('hidden');
+		price.focus();
+	})
+	price.focus();
+})();
